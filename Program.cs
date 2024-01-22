cef7f5705c244b6280e3fd446b640cfc4f75276e
etianm <155981372+etianm@users.noreply.github.com>
2024-01-22 09:31:03 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Vector
+{
+
+    int max = 0;
+    public class Peroana
+    {
+        string FirstName, LastName;
+        Console.Write("Input a FirstName: ");
+            FirstName = Console.ReadLine();
+        Console.Write("Input a LastName: ");
+            LastName = Console.ReadLine();
+        DateTime YearOfBirth;
+        YearOfBirth = Console.ReadLine();
+        int Age;
+        Age = DateTime.Now.Year - YearOfBirth;
+        if (Age > max) max = Age;
+    }
+
+}
+
